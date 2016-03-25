04479fc8181b95d324a5d3c02a274461e3064174
vaudevillian <vaudevillian@web.de>
2016-03-25 10:58:32 +0100

    GameManager added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+
+
+	// Use this for initialization
+	void Awake ()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);	
+	}
+	
+    void InitGame()
+    {
+
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
